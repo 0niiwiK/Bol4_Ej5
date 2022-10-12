805060c54b82135f12378c770911977eae1da672
Kiwii <israsanfuen@gmail.com>
2022-10-13 01:27:33 +0200

    Ej 5 terminado

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int num1, num2, aux;
+    printf("Introduzca un numero entero: ");
+    scanf("%d",&num1);
+    fflush(stdin);
+    printf("Introduzca otro numero entero distinto al anterior: ");
+    scanf("%d",&num2);
+    fflush(stdin);
+    while (num1==num2){
+        printf("Los numeros son iguales");
+        printf("\nIntroduzca un numero distinto al anterior: ");
+        scanf("%d",&num2);
+        fflush(stdin);
+    }
+    if (num1>num2){
+        aux=num1;
+        num1=num2;
+        num2=aux;
+    }
+    do {
+        printf("%d ",num1);
+        num1=num1+2;
+    } while (num1<=num2);
     return 0;
 }
